626471e67d510d83673a779d952720406cbc6d3f
Stefanos Gkouveris <stefanosgkouveris@itg.co.uk>
2021-08-09 20:11:55 +0100

    Added comments
    - Added comments to the code where needed
    - Added favicon
    - Minor UI change

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+//Define variables
 const shuffleButton = document.querySelector("#shuffle_btn");
 const sortButton = document.querySelector("#sort_btn");
 const main = document.querySelector("#main");
 
+//Initiate the Array
 let cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+//Shuffling function based on Fisher-Yates algorithm
 const shuffleArray = array => {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor( Math.random() * ( i + 1 ) );
@@ -15,10 +18,12 @@ const shuffleArray = array => {
     cards = array;
 };
 
+//Sort function to sort on ascending order
 const sortArray = () => {
     cards.sort();
 };
 
+//Function for the DOM manipulation
 const assignArray = array => {
     main.innerHTML = "";
 
@@ -27,14 +32,17 @@ const assignArray = array => {
     })
 }
 
+//Listen to the Shuffle button and execute the array shuffle and DOM restructure
 shuffleButton.addEventListener("click",  () => {
     shuffleArray(cards);
     assignArray(cards);
 });
 
+//Listen to the Sort button and execute the array sorting and DOM restructure
 sortButton.addEventListener("click",  () => {
     sortArray();
     assignArray(cards);
 });
 
+//Initiate the DOM for the first browser load
 assignArray(cards);
